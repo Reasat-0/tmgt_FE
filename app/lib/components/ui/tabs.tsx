98b2df8db3f7faddd11d2f7651da7d041b38df6d
Reasat <reasat.g8@gmail.com>
2026-06-11 21:07:09 +0600

    feat: addition of some files

--- app/lib/components/ui/tabs.tsx
+++ app/lib/components/ui/tabs.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { Tabs as TabsPrimitive } from "radix-ui";
 import * as React from "react";
 
-import { cn } from "../../utils/utils";
+import { cn } from "@/app/lib/utils/utils";
 
 function Tabs({
   className,
